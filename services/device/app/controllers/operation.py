4436642dd40bf73933c810964d152523814b2a04
CR1337 <captain_1@gmx.de>
2022-01-07 20:01:01 +0100

    added controllers

---
+++ services/device/app/controllers/operation.py
@@ -0,0 +1,49 @@
+class OperationController():
+
+    @classmethod
+    def set_program(cls, program):
+        ...
+
+    @classmethod
+    def delete_program(cls):
+        ...
+
+    @classmethod
+    def run_program(cls):
+        ...
+
+    @classmethod
+    def pause_program(cls):
+        ...
+
+    @classmethod
+    def continue_program(cls):
+        ...
+
+    @classmethod
+    def stop_program(cls):
+        ...
+
+    @classmethod
+    def schedule_program(cls):
+        ...
+
+    @classmethod
+    def unschedule_program(cls):
+        ...
+
+    @classmethod
+    def fire(cls):
+        ...
+
+    @classmethod
+    def lock(cls):
+        ...
+
+    @classmethod
+    def unlock(cls):
+        ...
+
+    @classmethod
+    def testloop(cls):
+        ...
---
+++ services/device/app/controllers/system.py
@@ -0,0 +1,13 @@
+class SystemController():
+
+    @classmethod
+    def set_system_time(cls, system_time):
+        ...
+
+    @classmethod
+    def register_master(cls):
+        ...
+
+    @classmethod
+    def deregister_master(cls):
+        ...

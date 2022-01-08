27051b25ce6595dee24c5e7739fc2cfcaeedf0f9
CR1337 <captain_1@gmx.de>
2022-01-08 13:27:02 +0100

    completed SystemController

--- services/device/app/controllers/system.py
+++ services/device/app/controllers/system.py
@@ -1,21 +1,42 @@
+from ..util.system_time import get_system_time, set_system_time
+from ..util.exceptions import RLException
+
+
+class MasterAlreadyRegistered(RLException):
+    pass
+
+
+class NoMasterRegistered(RLException):
+    pass
 
 
 class SystemController():
 
-    _connected_master = None
+    _connected_master_ip = None
 
     @classmethod
-    def set_system_time(cls, system_time):
-        ...
+    def change_system_time(cls, system_time):
+        set_system_time(system_time)
 
     @classmethod
-    def register_master(cls):
-        ...
+    def register_master(cls, master_ip):
+        if cls._connected_master_ip is not None:
+            raise MasterAlreadyRegistered()
+        cls._connected_master_ip = master_ip
 
     @classmethod
     def deregister_master(cls):
-        ...
+        if cls._connected_master_ip is None:
+            raise NoMasterRegistered()
+        cls._connected_master_ip = None
 
     @classmethod
     def get_status(cls):
-        return None
+        return {
+            'system_time': get_system_time(),
+            'connected_master_ip': cls._connected_master_ip
+        }
+
+    @classmethod
+    def get_connected_master_ip(cls):
+        return cls._connected_master_ip

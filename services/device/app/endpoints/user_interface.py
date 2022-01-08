3b029d67a1edd3902c0a4ae4f14339ed891da01a
CR1337 <captain_1@gmx.de>
2022-01-08 13:02:26 +0100

    added status-stream endpoint

--- services/device/app/endpoints/user_interface.py
+++ services/device/app/endpoints/user_interface.py
@@ -1,5 +1,10 @@
-from flask import Blueprint, request, render_template, send_from_directory
+from flask import (Blueprint, Response, render_template, request,
+                   send_from_directory)
 from flask_api import status
+
+import time
+import json
+from itertools import count
 
 from .util import handle_exceptions
 
@@ -14,6 +19,35 @@ user_iterface_blueprint = Blueprint('user_interface_blueprint', __name__)
 @handle_exceptions
 def ep_root():
     return render_template('user_interface.html')
+
+
+@user_iterface_blueprint.route(
+    "/status-stream",
+    methods=['GET'],
+    endpoint='ep_status_stream'
+)
+def ep_status_stream():
+    def status_stream():
+        for i in count(start=0):
+            time.sleep(0.5)
+            # TODO:
+            data = {
+                'system_status': {
+                    'system_time': None,
+                    'connected_master_ip': None
+                },
+                'operation_status': {
+                    'loaded_program': None,
+                    'program_state': None,
+                    'fuse_states': [None],
+                    'locked': None
+                }
+            }
+            yield f"data: {json.dumps(data)}\nid: {str(i)}\n\n"
+    return Response(
+        status_stream(),
+        mimetype="text/event-stream"
+    )
 
 
 @user_iterface_blueprint.route(

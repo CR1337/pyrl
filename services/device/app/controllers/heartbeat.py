ae0a62c8bb32af865878e1d53d8526badb1c07d9
CR1337 <captain_1@gmx.de>
2022-01-09 19:40:07 +0100

    completed HeartbeatController

--- services/device/app/controllers/heartbeat.py
+++ services/device/app/controllers/heartbeat.py
@@ -1,9 +1,49 @@
+from threading import Thread
+import threading
+import time
+
+import requests
+from .operation import OperationController
+from .system import SystemController
+
+
 class HeartbeatController():
+
+    THREAD = None
+    STOP_EVENT = threading.Event()
 
     @classmethod
     def start(cls):
-        ...
+        cls.THREAD = Thread(
+            target=cls._heartbeat_handler,
+            name='heartbeat_handler_thread'
+        )
+        cls.THREAD.start()
 
     @classmethod
     def stop(cls):
-        ...
+        cls.STOP_EVENT.set()
+        cls.THREAD.join()
+        cls.THREAD = None
+        cls.STOP_EVENT.clear()
+
+    @classmethod
+    def _heartbeat_handler(cls):
+        while not cls.STOP_EVENT.is_set():
+            heartbeat_data = {
+                'operation_status': OperationController.get_status(),
+                'system_status': OperationController.get_status()
+            }
+            master_url = (
+                f"http://{SystemController.get_connected_master_ip()}"
+                f"/device-interface/heartbeat"
+            )
+            try:
+                response = requests.post(
+                    url=master_url,
+                    json=heartbeat_data
+                )
+                response.raise_for_status()
+            except requests.exceptions.RequestException:
+                ...  # TODO: log
+            time.sleep(0.5)

9db2cb28639e038bda2b418e3f60140e74d4de14
CR1337 <captain_1@gmx.de>
2022-01-11 19:32:24 +0100

    added app.run

---
+++ services/device/start.py
@@ -0,0 +1,22 @@
+from app import app
+from os import environ
+
+if __name__ == "__main__":
+    if bool(environ.get('PYRL_DEBUG', False)):
+        app.run(
+            host='0.0.0.0',
+            port=80,
+            debug=True,
+            use_reloader=True,
+            use_debugger=True,
+            use_evalex=True,
+            threaded=False,
+        )
+    else:
+        app.run(
+            host='0.0.0.0',
+            pot=80,
+            debug=False,
+            threaded=True,
+            processes=4
+        )
